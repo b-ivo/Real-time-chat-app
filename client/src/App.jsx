ed5e04c0820ca91ee4b3240f9e9e21d10be6a8be
Ivo  Bryan <ivobryan12@gmail.com>
2025-11-23 15:44:58 +0200

    fafhv

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3001");
+const socket = io("https://real-time-chat-app-bq55.onrender.com");
 
 function App() {
   const [username, setUsername] = useState("");// set the setUsername
